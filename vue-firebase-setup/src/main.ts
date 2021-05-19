587cd7b7460f1be98167a996ba3a32189909df51
Yusuke <yusuke_dam41@hotmail.com>
2021-05-19 15:30:02 +0900

    初期設定

---
+++ vue-firebase-setup/src/main.ts
@@ -0,0 +1,9 @@
+import { createApp } from 'vue'
+import App from './App.vue'
+import router from './router'
+import store from './store'
+import { firestorePlugin } from 'vuefire'
+
+createApp(App)
+.use(firestorePlugin)
+.mount('#app')
